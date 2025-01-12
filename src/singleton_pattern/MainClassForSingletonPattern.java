8c962a699b755d10f4d120aed7f0f1b6489a8931
eranga2000 <dmemadhushan@std.appsc.sab.ac.lk>
2025-01-12 23:38:26 +0530

    strategy patter
    Signed-off-by: eranga2000 <dmemadhushan@std.appsc.sab.ac.lk>

--- src/singleton_pattern/MainClassForSingletonPattern.java
+++ src/singleton_pattern/MainClassForSingletonPattern.java
@@ -3,8 +3,7 @@ package singleton_pattern;
 public class MainClassForSingletonPattern {
     public static void main(String[] args) {
         SingletonClass hi= SingletonClass.getSingleObj("db");
-        SingletonClass hi2=
-        SingletonClass.getSingleObj("45646");
+        SingletonClass hi2= SingletonClass.getSingleObj("45646");
         if(hi==hi2){
             System.out.println("same");
         }
